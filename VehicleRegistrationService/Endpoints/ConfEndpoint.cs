6070c8f45901670b2f44946f741e4f8d1ad7cc8c
Jonathon Douglas <jdouglas@acquisinsurance.com>
2022-10-11 16:49:57 +0100

    feat: hidden endpoints

---
+++ VehicleRegistrationService/Endpoints/ConfEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace VehicleRegistrationService.Endpoints
+{
+    using System.Text.Json;
+
+    public class ConfEndpoint : EndpointWithoutRequest
+    {
+        public override void Configure()
+        {
+            Get("/conf");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            IConfiguration? allConfig = Resolve<IConfiguration>();
+            IEnumerable<KeyValuePair<string, string>> configKv = allConfig.AsEnumerable();
+
+            var jsonSerializerOptions = new JsonSerializerOptions { WriteIndented = true };
+
+
+            await SendAsync(configKv, 200, ct).ConfigureAwait(false);
+        }
+    }
+}
