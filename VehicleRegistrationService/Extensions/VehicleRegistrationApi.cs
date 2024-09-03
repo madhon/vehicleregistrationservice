0251213c6ae447462603540485c06604cf3b6866
Jonathon Douglas <jdouglas@acquisinsurance.com>
2024-09-03 16:55:02 +0100

    feat: move to AzureLinux from Alpine and remove requirement for icu

--- VehicleRegistrationService/Extensions/VehicleRegistrationApi.cs
+++ VehicleRegistrationService/Extensions/VehicleRegistrationApi.cs
@@ -6,6 +6,8 @@ public static class VehicleRegistrationApi
 {
     public static RouteGroupBuilder MapVehicleRegistrationApi(this IEndpointRouteBuilder routes)
     {
+        routes.MapGet("/", () => TypedResults.LocalRedirect("~/docs"));
+
         var group = routes.MapGroup("api/v1/");
 
         group.MapLoginEndpoint();
